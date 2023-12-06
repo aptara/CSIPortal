9befd7301efef250715a341f682a6e34ba1f2e9f
PushprajJagadale <Pushpraj.Jagadale@aptaracorp.com>
2023-12-06 20:27:15 +0530

    Environment.prod.ts

--- Portal/src/app/Environment.prod.ts
+++ Portal/src/app/Environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
-
     
     production: true,
     UAT: false,
     Local: false,
-    baseURL: 'https://ctsdemo.aptaracorp.net/CSIPortal/',
-    baseUrl: 'https://ctsdemo.aptaracorp.net/CSIPortalAPI/'
+    baseURL: 'http://151.106.38.69/',
+    baseUrl: 'https://ctsdemo.aptaracorp.net/CSIPortalAPI/api/'
 
 };
--- Portal/src/app/Environment.ts
+++ Portal/src/app/Environment.ts
@@ -1,11 +1,8 @@
 export const environment = {
-    production: false,
+    production: true,
     UAT: false,
     Local: true,
-    baseUrl: 'http://localhost:62220/API/',
-    Prod :''
-    //ssoProd: 'https://msvcs.us.kworld.kpmg.com/lmsmicroapp/kpmglrs/api/SingleSignOn',
-    //ssoUAT: 'https://msvcsstg1.us.kworld.kpmg.com/lmsmicroappuat/kpmglrs/api/SingleSignOn'
-
-    // baseUrl: 'http://ctsdemo.aptaracorp.net//AdvisoryDatabaseAPI/API/'
+    baseURL: 'http://151.106.38.69/',
+    baseUrl: 'https://ctsdemo.aptaracorp.net/CSIPortalAPI/api/'
+   
 };
